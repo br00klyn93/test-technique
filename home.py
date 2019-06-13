18c42f2c4e7c22c76f9e2519379e1cb4312c5d03
br00klyn93 <bmclaury93@gmail.com>
2019-06-13 07:50:08 -0700

    Create home.py

---
+++ home.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, request, render_template, redirect
+from flask import make_response, Response
+import base64
+# import GrammarChecker as g
+import time
+import json
+import requests
+# from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/app/static/data'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+
+LD_LIBRARY_PATH='/app'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=["POST"])
+def main():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['image']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = "test.png"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+            
+    return render_template("index.html")
